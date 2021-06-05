fcee0cea13469080d06c39df3ede8ae7baeb30ad
Jorge_Jim <jorge7112000@hotmail.com>
2021-06-05 11:17:54 -0500

    Initial commit

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,13 +5,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>EpicPelis</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+        <link rel="shortcut icon" href="https://i.ibb.co/CmWZvGm/Logo-Epic-Pelis.png" />
 
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
