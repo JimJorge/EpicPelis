ceb1de1b9d8cdec26ee2d9cf66d1ff537e972097
Jorge_Jim <jorge7112000@hotmail.com>
2021-06-16 19:35:46 -0500

    Initial commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,48 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+        <h2 class="font-semibold text-xl text-center text-gray-800 leading-tight " id="texto_centro">
+            Bienvenido
         </h2>
     </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <section class="custom">
+                <div class="sing-colm">
+                    <div class="row content-movie">
+                        <div class="content-img">
+                            <div id="posterImg">
+                                <img id="poster" src="./Films Hunter_files/s7hV8HFh7w2EZ4GK5EiltRVvWJA.jpg" srcset="https://image.tmdb.org/t/p/w300_and_h450_bestv2/s7hV8HFh7w2EZ4GK5EiltRVvWJA.jpg 1x, https://image.tmdb.org/t/p/w600_and_h900_bestv2/s7hV8HFh7w2EZ4GK5EiltRVvWJA.jpg 2x" alt="Thor: Ragnarok">"&gt;
+                            </div>
+                        </div>
+                        <div id="info-movie">
+
+                            <section id="boxContent">
+                                <div class="box-title">
+                                    <h2 id="title">Thor: Ragnarok</h2>
+                                    <span id="year">(2017)</span>
+                                </div>
+                                <div class="box-title">
+                                    <h2 id="genero">Genéro</h2>
+                                    <span id="timer">(104 min)</span>
+                                </div>
+                                <div class="box-desciption">
+                                    <h5>General</h5>
+                                    <p>Thor está preso al otro lado del universo sin su poderoso martillo y se enfrenta a una carrera contra el tiempo. Su objetivo es volver a Asgard y parar el Ragnarok porque significaría la destrucción de su planeta natal y el fin de la civilización Asgardiana a manos de una todopoderosa y nueva amenaza, la implacable Hela. Pero, primero deberá sobrevivir a una competición letal de gladiadores que lo enfrentará a su aliado y compañero en los Vengadores, ¡el Increíble Hulk!.</p>
+
+                                </div>
+                            </section>
+
+
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+
             </div>
         </div>
     </div>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,14 +6,14 @@
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                        <img src="https://i.ibb.co/CmWZvGm/Logo-Epic-Pelis.png">
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        Inicio
                     </x-nav-link>
                 </div>
             </div>
@@ -64,7 +64,7 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+                Inicio
             </x-responsive-nav-link>
         </div>
 
@@ -83,7 +83,7 @@
                     <x-responsive-nav-link :href="route('logout')"
                             onclick="event.preventDefault();
                                         this.closest('form').submit();">
-                        {{ __('Log Out') }}
+                        Cerrar Sesion
                     </x-responsive-nav-link>
                 </form>
             </div>
